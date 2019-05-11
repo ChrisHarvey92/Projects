aa99b93e7d36ae00a2e5f4ca88fb6da4fd9352c1
ChrisHarvey92 <45005260+ChrisHarvey92@users.noreply.github.com>
2019-05-11 22:44:28 +0100

    Nice Clean Code!

---
+++ Fresh2DrPG/Assets/Scripts/characterController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    public float MOVEMENT_BASE_SPEED = 1.0f;
+
+    public Rigidbody2D rb;
+
+    private GameObject playerGO;
+    private SpriteRenderer playerSR;
+    private Animator wizAnim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerGO = GameObject.FindGameObjectWithTag("Player");
+        playerSR = playerGO.GetComponent<SpriteRenderer>();
+        playerSR.flipX = false;
+        wizAnim = playerGO.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+        FlipSprite();
+    }
+
+    private void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * MOVEMENT_BASE_SPEED;
+    }
+
+    private void ProcessInputs()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    private void FlipSprite()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            playerSR.flipX = true;
+            wizAnim.Play("Wizard_Run");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            playerSR.flipX = false;
+            wizAnim.Play("Wizard_Run");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            wizAnim.Play("Wizard_Attack");
+        }
+
+        if (Input.anyKey == false)
+        {
+            wizAnim.Play("Wizard_Idle");
+        }
+    }
+}
--- Fresh2DrPG/Assets/Scripts/mageSkills.cs
+++ Fresh2DrPG/Assets/Scripts/mageSkills.cs
@@ -8,23 +8,23 @@ public class mageSkills : MonoBehaviour
 
     private Image fireboltIcon, firewallIcon, voidburstIcon, electricpulseIcon, lightauraIcon, speedauraIcon, powerauraIcon, darkauraIcon, skillBar;
     private GameObject skillScroll;
-    private SpriteRenderer scrollRenderer;
+    private Canvas scrollRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        fireboltIcon = Resources.Load<Image>("Images/firebolt");
-        firewallIcon = Resources.Load<Image>("Images/firewall");
-        voidburstIcon = Resources.Load<Image>("Images/voidburst");
-        electricpulseIcon = Resources.Load<Image>("Images/electricpulse");
-        lightauraIcon = Resources.Load<Image>("Images/lightaura");
-        speedauraIcon = Resources.Load<Image>("Images/speedaura");
-        powerauraIcon = Resources.Load<Image>("Images/poweraura");
-        darkauraIcon = Resources.Load<Image>("Images/darkaura");
+        fireboltIcon = Resources.Load<Image>("Icons/firebolt");
+        firewallIcon = Resources.Load<Image>("Icons/firewall");
+        voidburstIcon = Resources.Load<Image>("Icons/voidburst");
+        electricpulseIcon = Resources.Load<Image>("Icons/electricpulse");
+        lightauraIcon = Resources.Load<Image>("Icons/lightaura");
+        speedauraIcon = Resources.Load<Image>("Icons/speedaura");
+        powerauraIcon = Resources.Load<Image>("Icons/poweraura");
+        darkauraIcon = Resources.Load<Image>("Icons/darkaura");
 
         skillBar = GetComponent<Image>();
         skillScroll = GameObject.FindGameObjectWithTag("skillScroll");
-        scrollRenderer = skillScroll.GetComponent<SpriteRenderer>();
+        scrollRenderer = skillScroll.GetComponent<Canvas>();
     }
 
     // Update is called once per frame
